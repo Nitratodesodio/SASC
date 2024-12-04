8d5817b8d7528e82063bdb588eea214884610e0d
Nano <nanoantillanca2@gmail.com>
2024-12-04 00:43:29 -0300

    Agregar settings a urls.py en core

--- Pw/core/urls.py
+++ Pw/core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import  DashboardView, InformeView, AlertaView
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
