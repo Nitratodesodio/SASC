a5fcd9991d2d782ce01fa2078078c80383a482cb
Diego Aedo Fuenzalida <diego.aedo08@inacapmail.cl>
2024-12-09 19:53:57 -0300

    agregar carga de datos del excel de planificación a las funciones de app administración

--- Pw/administracion/views.py
+++ Pw/administracion/views.py
@@ -1,1 +1,10 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .utils import cargar_datos
 # Create your views here.
+@login_required()
+def carga_data(request):
+    if request.method == 'POST':
+        archivo = request.FILES['file']
+        cargar_datos(archivo)
+    return render(request, 'carga_datos.html')
