1067cdf6a3a1c92d0ab8de23ec729b1f63ad870a
Diego Aedo Fuenzalida <diego.aedo08@inacapmail.cl>
2024-12-09 05:15:39 -0300

    habilitar cargo y sede en UsuarioCreationForm

--- Pw/autenticacion/forms.py
+++ Pw/autenticacion/forms.py
@@ -7,17 +7,21 @@ class UsuarioCreationForm(BaseUserCreationForm):
 
     class Meta:
         model = Usuario
-        fields = ['rut', 'nombre', 'email']
+        fields = ['rut', 'nombre', 'email', 'cargo', 'sede']
         labels = {
             'rut': 'Rut',
             'nombre': 'Nombre y apellidos',
             'email': 'Correo electrónico',
+            'cargo': 'Cargo',
+            'sede': 'Sede'
 
         }
         widgets = {
             'rut': TextInput(attrs={'class': 'form-control'}),
             'nombre': TextInput(attrs={'class': 'form-control'}),
             'email': EmailInput(attrs={'class': 'form-control'}),
+            'cargo': Select(choices=Cargo.objects.all(),attrs={'class': 'form-control'}),
+            'sede': Select(choices=Sede.objects.all(),attrs={'class': 'form-control'}),
 
         }
 
